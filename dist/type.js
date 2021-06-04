4a77b9d242307d41e3c0edee3f44ebf38d167dc7
mdemon420 <mdeamon4@gmail.com>
2021-06-04 23:58:16 +0600

    add array and object

--- dist/type.js
+++ dist/type.js
@@ -20,3 +20,26 @@ console.log(multiply(10, 5));
 let multiply2;
 multiply2 = (x, y) => x * y;
 console.log(multiply2(4, 4));
+// array
+let numbers = [10, 20, 30, 40, 50, 60];
+numbers.push(70);
+const friends = ["Najim", "Akter", "Muktader", "Saddam"];
+let megaName = "";
+for (let i = 0; i < friends.length; i++) {
+    let friend = friends[i];
+    if (friend.length > megaName.length) {
+        megaName = friend;
+    }
+}
+console.log("Friend with the largest name", megaName);
+// object
+const friend = {
+    name: "Faruk Zaman",
+    age: 31
+};
+friend.age = 33;
+let player;
+player = {
+    club: "barsa",
+    salary: 2000,
+};
