33f01ece42b9f41e92892722298f55dd273e4c9f
mdemon420 <mdeamon4@gmail.com>
2021-06-04 23:57:25 +0600

    add array and object

--- type.ts
+++ type.ts
@@ -25,3 +25,39 @@ console.log(multiply(10,5)) ;
 let multiply2: (x:number, y:number )=>number;
 multiply2 = (x ,y ) =>x*y;
 console.log(multiply2(4,4));
+
+
+// array
+
+let numbers : number[] = [10,20,30,40,50,60];
+numbers.push(70);
+
+const friends : string[] = ["Najim","Akter","Muktader","Saddam"];
+let megaName: string = ""
+for(let i = 0; i<friends.length; i++){
+    let friend: string = friends[i];
+    if(friend.length > megaName.length){
+        megaName= friend;
+    }
+}
+
+console.log("Friend with the largest name", megaName);
+
+
+// object
+
+const friend:{name:string,age:number} ={
+    name: "Faruk Zaman",
+    age:31
+}
+friend.age= 33;
+
+let player:{
+    club:string,
+    salary:number
+}
+
+player={
+    club: "barsa",
+    salary: 2000,
+}
